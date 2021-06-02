9003f59c4634e0fd98e33a6a11bf75caa9e644ff
xxx-xz <torgl1598@gmail.com>
2021-06-02 16:39:25 +0900

    DBConn

---
+++ Subway/src/DBConn/DBConn.java
@@ -0,0 +1,44 @@
+package DBConn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBConn {
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet  rs;
+	String url = "jdbc:oracle:thin:@localhost:1521";
+	String user = "scott";
+	String pass = "tiger";
+	
+	public DBConn() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url,user,pass);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getPreparedStatement(String sql) {
+		try {
+			pstmt = conn.prepareStatement(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
