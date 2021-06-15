d9a3059f0703095d1dd8d0407aa64520b79e6c78
user <user@PC00>
2021-06-15 16:24:41 +0900

    16:24 Final OrderVO 생성

---
+++ Subway/src/com/subway/vo/FinalOrderVO.java
@@ -0,0 +1,51 @@
+package com.subway.vo;
+
+public class FinalOrderVO {
+	String  O_NO, O_EAT, HP, O_MESSAGE,  O_COUPON;          
+	int O_AMT,  O_PRICE;
+	
+	public String getO_NO() {
+		return O_NO;
+	}
+	public void setO_NO(String o_NO) {
+		O_NO = o_NO;
+	}
+	public String getO_EAT() {
+		return O_EAT;
+	}
+	public void setO_EAT(String o_EAT) {
+		O_EAT = o_EAT;
+	}
+	public String getHP() {
+		return HP;
+	}
+	public void setHP(String hP) {
+		HP = hP;
+	}
+	public String getO_MESSAGE() {
+		return O_MESSAGE;
+	}
+	public void setO_MESSAGE(String o_MESSAGE) {
+		O_MESSAGE = o_MESSAGE;
+	}
+	public String getO_COUPON() {
+		return O_COUPON;
+	}
+	public void setO_COUPON(String o_COUPON) {
+		O_COUPON = o_COUPON;
+	}
+	public int getO_AMT() {
+		return O_AMT;
+	}
+	public void setO_AMT(int o_AMT) {
+		O_AMT = o_AMT;
+	}
+	public int getO_PRICE() {
+		return O_PRICE;
+	}
+	public void setO_PRICE(int o_PRICE) {
+		O_PRICE = o_PRICE;
+	}
+	
+	    
+}
